1bfe3a344b78af990c010a7f3537a740c9814920
Daniel Ragazzo <dragazzo01@outlook.com>
2025-08-19 16:03:14 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/serial-bowl/sw.js', { scope: '/serial-bowl/' })})}
